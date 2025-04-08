4b7a3f09d85b74e8764505344bab41d08991308b
yCo-0601 <yco240616@gmail.com>
2025-04-08 09:13:30 +0000

    Deploying to gh-pages from @ yCo-0601/nav@d8eac2db66941fbcf665b2baeb69e75da56680d2 🚀

---
+++ chunk-DAPGHKKF.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-TO3OKCAY.js";import{Ab as e,qb as r}from"./chunk-SVS3H4GC.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
